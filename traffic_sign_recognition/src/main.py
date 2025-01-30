78011709dc63b89396a99093d35d76ca21d32d06
Lolillkillu <szymon.parusmajor@gmail.com>
2025-01-30 17:03:03 +0100

    Remove redundant files and add functionality to main.py

--- traffic_sign_recognition/src/main.py
+++ traffic_sign_recognition/src/main.py
@@ -1,10 +1,19 @@
 from ultralytics import YOLO
 import argparse
+import os
 
-if __name__ == "main":
-    parser = argparse.ArgumentParser
+
+def print_detected_sign(results):
+    for result in results:
+        for box in result.boxes:
+            class_id = int(box.cls)
+            class_name = result.names[class_id]
+            print(f"Rozpoznano: {class_name}")
+
+def main():
+    parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help = "Input file/stream for predicting OR YAML for dataset to train on", required = True)
-    parser.add_argument("-m", "--model", help = "File of model to use. If training, select the pretrained model. Default = ../model/model.pt", default = "../model/model.pt")
+    parser.add_argument("-m", "--model", help = "File of model to use. If training, select the pretrained model. Default = ../model/best.pt", default = "../model/best.pt")
     parser.add_argument("-t", "--train", help = "Train model", default = False)
     args = parser.parse_args()
     
@@ -30,4 +39,24 @@ if __name__ == "main":
         exit()
 
     model = YOLO(args.model)
-    model.predict(args.input, save = True)
+
+    file_path = args.input
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Plik {file_path} nie istnieje.")
+
+    ext = os.path.splitext(file_path)[1].lower()
+    if ext in ['.jpg', '.jpeg', '.png', '.bmp', '.gif']:
+        print("Przetwarzanie obrazu...")
+    elif ext in ['.mp4', '.avi', '.mov', '.mkv']:
+        print("Przetwarzanie filmu...")
+    else:
+        raise ValueError("Nieobsługiwany format pliku. Użyj innego pliku.")
+
+    results = model.predict(file_path, save=True)
+    
+    print_detected_sign(results)
+    
+    print("Koniec UwU")
+
+if __name__ == "__main__":
+    main()
